0baaa30d8282f2f52aea25854c8f237de87bb1f8
Satyam <satyam@satyam.com.ar>
2021-02-15 17:39:30 +0100

    re-export everything, including TS types

--- src/Form/index.ts
+++ src/Form/index.ts
@@ -1,9 +1,9 @@
-export { default as TextField } from './TextField';
-export { default as CurrencyField } from './CurrencyField';
-export { default as PercentField } from './PercentField';
-export { default as DateField } from './DateField';
-export { default as CheckboxField } from './CheckboxField';
-export { default as DropdownField } from './DropdownField';
-export { default as SubmitButton } from './SubmitButton';
-export { LabeledCheckbox, LabeledText } from './LabeledField';
-export { default as RadioField } from './RadioField';
+export * from './TextField';
+export * from './CurrencyField';
+export * from './PercentField';
+export * from './DateField';
+export * from './CheckboxField';
+export * from './DropdownField';
+export * from './SubmitButton';
+export * from './LabeledField';
+export * from './RadioField';
